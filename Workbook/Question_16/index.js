cf3fa0ebbfb1100c7727dd083f058bc78aa82e32
OZahnitko <6964653+OZahnitko@users.noreply.github.com>
2021-02-24 07:26:31 +1000

    Question 16.
    Better.

--- Workbook/Question_16/index.js
+++ Workbook/Question_16/index.js
@@ -1,140 +1,115 @@
+const chalk = require("chalk");
 const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
+  terminal: false,
 });
 
 const skillsData = [
   { codingSkill: "Python", weight: 1 },
   { codingSkill: "Ruby", weight: 2 },
-  { codingSkill: "bash", weight: 4 },
-  { codingSkill: "git", weight: 8 },
-  { codingSkill: "html", weight: 16 },
-  { codingSkill: "tdd", weight: 32 },
-  { codingSkill: "css", weight: 64 },
+  { codingSkill: "Bash", weight: 4 },
+  { codingSkill: "Git", weight: 8 },
+  { codingSkill: "HTML", weight: 16 },
+  { codingSkill: "TDD", weight: 32 },
+  { codingSkill: "CSS", weight: 64 },
   { codingSkill: "JavaScript", weight: 128 },
 ];
 //TODO: Check only the individual items, i.e. maybe a user made a mistake in a single string, no need to reject the whole.
 //TODO: Check for empty input strings
 //TODO: Allow for multiple inputs - don't close stdin
 
-const parseInput = (rawInput) => {
-  //   return /^[A-Za-z\s,]+$/.test(rawInput)
-  //     ? {
-  //         data: rawInput
-  //           .trim()
-  //           .replace(/,+/g, "")
-  //           .split(/\s+/)
-  //           .map((skill) => skill.toLowerCase()),
-  //       }
-  //     : { error: "Invalid input" };
-  //   return {
-  //     data: rawInput
-  //       .trim()
-  //       .replace(/,+/g, " ")
-  //       .split(/\s+/)
-  //       .map((userSkill) => ({
-  //         skillName: userSkill,
-  //         valid:
-  //           (/^[A-Za-z\s,]+$/.test(userSkill) &&
-  //             skillsData.find(
-  //               (skill) =>
-  //                 skill.codingSkill.toLowerCase() === userSkill.toLowerCase()
-  //             )) ||
-  //           false,
-  //       })),
-  //   };
-  return {
-    data: rawInput
-      .trim()
-      .replace(/,+/g, " ")
-      .split(/\s+/)
-      .map((userSkill) => {
-        const targetSkill = skillsData.find(
-          (skill) => skill.codingSkill.toLowerCase() === userSkill.toLowerCase()
-        );
-        return targetSkill
-          ? targetSkill
-          : {
-              codingSkill: userSkill,
-              message: `Could not find information about ${userSkill} in our database, and it's value is not reflected in your score.`,
-              weight: null,
-            };
-      }),
-  };
-};
-
-// rl.question(
-//   "Please enter your skills, separated by either space or a comma: ",
-//   (userInput) => {
-//     rl.close();
-//     const userSkills = parseInput(userInput);
-//     console.log(userSkills);
-//     // console.log(
-//     //   userSkills.map((userSkill) => {
-//     //     const targetSkill = skillsData.find(
-//     //       (skill) => skill.codingSkill.toLowerCase() === userSkill
-//     //     );
-//     //     console.log(targetSkill);
-//     //     return `You're good at ${targetSkill.codingSkill}, which adds ${targetSkill.weight} to your score.`;
-//     //   })
-//     // );
-//   }
-// );
-
-// rl.question(
-//   "Please enter your coding skills, separated by space or comma: ",
-//   (answer) => {
-//     console.log(answer.trim().length);
-//     rl.close();
-//   }
-// );
-
-// const askQuestion = () => {
-//   const finished = false;
-//   rl.question(
-//     "Please enter your coding skills, separated by space or comma: ",
-//     (answer) => {
-//       rl.close();
-//       console.log(answer.trim().length);
-//       if (answer.trim().length === 0) {
-//         finished = false;
-//       } else {
-//         finished === true;
-//       }
-//     }
-//   );
-// if (finished) {
-//   askQuestion();
-// }
-// };
-
-// askQuestion();
-
-// const askQuestion = () => {
-//   const askAgain = false;
-
-//   rl.question("What?: ", (answer) => {
-//     rl.close();
-//     if (answer.trim().toLowerCase() === "what") {
-//       askAgain === true;
-//     }
-//   });
-
-//   console.log(`Ask again is set to ${askAgain}`);
-
-//   if (askAgain === true) {
-//     askQuestion();
-//   }
-// };
-
 const askQuestion = () => {
-  rl.question("What is the name: ", (answer) => {
-    console.log("And the answer is...");
-    console.log(answer);
-    return rl.close();
+  rl.question("Enter your coding skills: ", (answer) => {
+    if (
+      answer.trim().trim().length === 0 ||
+      answer.trim().replace(/,+/g, " ").trim().length === 0
+    ) {
+      console.log(
+        "You did not seem to enter anything there, samurai!\nTry Again!"
+      );
+      askQuestion();
+    } else {
+      formatOutput(parseInput(answer));
+      rl.question("\nDo you want to go again? (Y/n): ", (response) => {
+        if (response.trim().toLowerCase() === "y") {
+          askQuestion();
+        } else {
+          console.log("\nCatch you on the flippity flip!");
+          rl.close();
+        }
+      });
+    }
   });
 };
+
+const formatOrdinalNumbers = (number) => {
+  switch (number) {
+    case 1:
+      return "1st";
+    case 2:
+      return "2nd";
+    case 3:
+      return "3rd";
+    default:
+      return `${number}th`;
+  }
+};
+
+const formatOutput = (input) => {
+  let output = input
+    .map(
+      ({ codingSkill, weight }, index) =>
+        `The ${formatOrdinalNumbers(index + 1)} skill you entered was "${
+          weight ? chalk.green(codingSkill) : chalk.red(codingSkill)
+        }". ${
+          weight
+            ? chalk.bgGreen(`Your score increased by ${weight} points.`)
+            : chalk.bgRed(
+                "We don't need that trash, so it will not reflect your score."
+              )
+        }`
+    )
+    .join("\n")
+    .concat(
+      `\n\nYour total score is ${input.reduce((acc, { weight }) => {
+        return acc + weight;
+      }, 0)}.`
+    )
+    .concat(
+      "\n\nIf you wanted to increase your score even further, look into learning the following...\n\n"
+    )
+    .concat(
+      skillsData
+        .filter(
+          ({ codingSkill }) =>
+            !input
+              .map((userSkill) => userSkill.codingSkill)
+              .includes(codingSkill)
+        )
+        .map(
+          ({ codingSkill, weight }) =>
+            `${codingSkill}, which would increase your score by ${weight}`
+        )
+        .join("\n")
+    );
+
+  console.log(output);
+};
+
+const parseInput = (rawInput) =>
+  rawInput
+    .trim()
+    .replace(/,+/g, " ")
+    .split(/\s+/)
+    .map(
+      (userSkill) =>
+        skillsData.find(
+          ({ codingSkill }) =>
+            codingSkill.toLowerCase() === userSkill.toLowerCase()
+        ) || { codingSkill: userSkill, weight: 0 }
+    );
 
 askQuestion();
